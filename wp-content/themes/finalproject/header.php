f8c1a5715dc0b555e1638aed4f9504f058277b49
Viet Anh Le <mc.vietanh@gmail.com>
2022-04-03 15:58:47 -0400

    update logo

--- wp-content/themes/finalproject/header.php
+++ wp-content/themes/finalproject/header.php
@@ -20,7 +20,13 @@
   <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
+
+        <a class="navbar-brand" href="#">
+          <img style="width: 60px;"
+            src="<?php echo esc_url(home_url('wp-content/themes/finalproject/images/logo.jpeg')) ?>"
+            alt="">
+        </a>
+
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
           aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
